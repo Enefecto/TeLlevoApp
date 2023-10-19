ed7b6ee5cc542f85a4a7a91481e5251fc586939a
Camilo Romero <camids980@gmail.com>
2023-10-19 20:30:53 -0300

    UPDATED logical guards with redirections

--- src/app/ingresado.guard.ts
+++ src/app/ingresado.guard.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngresadoGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (localStorage.getItem('ingresado') == 'true'){
       return true;
     } else {
+      this.router.navigate(['/login']);
       return false;
     }
   }
--- src/app/no-ingresado.guard.ts
+++ src/app/no-ingresado.guard.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoIngresadoGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       if (localStorage.getItem('ingresado') == 'true'){
+        this.router.navigate(['/home']);
         return false;
       } else {
         return true;
