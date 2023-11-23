eade02c050ae079f61acf02c1076169cb002902f
Camilo Romero <camids980@gmail.com>
2023-11-23 19:56:47 -0300

    Android added

--- src/app/api-rest-service.service.ts
+++ src/app/api-rest-service.service.ts
@@ -6,42 +6,45 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiRestServiceService {
 
+  direccion: String = 'https://2qphs7jx-8000.brs.devtunnels.ms/';
+  //direccion: String = 'http://127.0.0.1:8000/';
+
   constructor(
     private http: HttpClient,
   ) { } 
 
   getUsuarios() {
-    const url = 'http://127.0.0.1:8000/api/usuarios/';
+    const url = `${this.direccion}/api/usuarios/`;
     return this.http.get(url);
   }
 
   crearUsuario(usuario: any) {
-    const url = 'http://127.0.0.1:8000/api/usuarios/';
+    const url = `${this.direccion}/api/usuarios/`;
     return this.http.post(url, usuario);
   }
 
   modificarUsuario(usuario :any, id: any){
-    const url = `http://127.0.0.1:8000/api/usuarios/${id}/`;
+    const url = `${this.direccion}/api/usuarios/${id}/`;
     return this.http.put(url,usuario);
   }
 
   getViajes(){
-    const url = 'http://127.0.0.1:8000/api/viajes/';
+    const url = `${this.direccion}/api/viajes/`;
     return this.http.get(url);
   }
 
   crearViaje(viaje: any){
-    const url = 'http://127.0.0.1:8000/api/viajes/';
+    const url = `${this.direccion}/api/viajes/`;
     return this.http.post(url,viaje);
   }
 
   actualizarViaje(viaje: any, id: number) {
-    const url = `http://127.0.0.1:8000/api/viajes/${id}/`;
+    const url = `${this.direccion}/api/viajes/${id}/`;
     return this.http.put(url, viaje);
   }
 
   eliminarViaje(id: number) {
-    const url = `http://127.0.0.1:8000/api/viajes/${id}/`;
+    const url = `${this.direccion}/api/viajes/${id}/`;
     return this.http.delete(url);
   }
 }
